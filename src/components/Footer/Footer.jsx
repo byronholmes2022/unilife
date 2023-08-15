3c39abb9c98b13628f83f15c8306107dfc9019b6
Byron Holmes <byronholmes@Byrons-MacBook-Pro.local>
2023-08-15 02:57:53 -0500

    fixed semicolons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,49 +1,54 @@
-import React from 'react'
-import './Footer.css'
-import { BsFacebook, BsInstagram, BsTwitter } from 'react-icons/bs'
+import React from "react";
+import "./Footer.css";
+import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
 
 function Footer() {
   return (
-    <div className='footer-container'>
+    <div className="footer-container">
       <div className="content-container">
         <div className="blue-block">
           <div className="email">
-          <h3>Keep in touch</h3>
-          <p className='email-p'>Curious about new offerings? Sign up for our weekly newsletter and stay informed.</p>
-          <input placeholder='Your email'></input>
+            <h3>Keep in touch</h3>
+            <p className="email-p">
+              Curious about new offerings? Sign up for our weekly newsletter and
+              stay informed.
+            </p>
+            <input placeholder="Your email"></input>
           </div>
-         <div className="social">
-          <h3>Let's Socialize</h3>
-          <div className="socials">
-            <a href='facebook.com' className="social-link">
-              <BsFacebook />
-              <p>Facebook</p>
-            </a>
-            <a href="instagram.com" className="social-link">
-              <BsInstagram/>
-              <p>Instagram</p>
-            </a>
-            <a href="twitter.com" className="social-link">
-              <BsTwitter/>
-              <p>Twitter</p>
-            </a>
+          <div className="social">
+            <h3>Let's Socialize</h3>
+            <div className="socials">
+              <a href="facebook.com" className="social-link">
+                <BsFacebook />
+                <p>Facebook</p>
+              </a>
+              <a href="instagram.com" className="social-link">
+                <BsInstagram />
+                <p>Instagram</p>
+              </a>
+              <a href="twitter.com" className="social-link">
+                <BsTwitter />
+                <p>Twitter</p>
+              </a>
+            </div>
           </div>
         </div>
-      </div>  
-      <div className="black-block">
-        <div className="left">
-          <p>About Us</p>
-          <p>Terms & Conditions</p>
-          <p>Privacy & Cookie Policies</p>
+        <div className="black-block">
+          <div className="left">
+            <p>About Us</p>
+            <p>Terms & Conditions</p>
+            <p>Privacy & Cookie Policies</p>
+          </div>
+          <div className="right">
+            <p>2022</p>
+            <p>
+              <span>&copy;</span> unilife
+            </p>
+          </div>
         </div>
-        <div className="right">
-          <p>2022</p>
-          <p><span>&copy;</span> unilife</p>
-        </div>
-        </div>
-       </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
